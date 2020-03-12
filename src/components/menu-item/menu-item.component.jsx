30b3ddf64672104717eb25c0d8fb105e5c6709a3
Mostafa <javascript.github@gmail.com>
2020-03-12 07:53:57 +0330

    broken our homepage into smaller components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./menu-item.styles.scss";
+export default function MenuItem({ title, size, imageUrl }) {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">Shop now</span>
+      </div>
+    </div>
+  );
+}
